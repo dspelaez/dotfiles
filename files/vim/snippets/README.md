d8e5fb55958549280592a4747e809a2a1ebcef86
Daniel Santiago <dspelaez@gmail.com>
2019-08-31 19:03:33 -0700

    new structure working well

--- files/vim/snippets/README.md
+++
@@ -1,1 +0,0 @@
-Most of the snippets were taken from [honza/vim-snippets](https://github.com/honza/vim-snippets/)
--- symlink.sh
+++ symlink.sh
@@ -6,3 +6,27 @@
 # Distributed under terms of the GNU/GPL license.
 
 
+DOTFILES=$HOME/.dotfiles
+CONFIG=$HOME/.config
+
+# latex
+ln -sf $DOTFILES/latex/latexmkrc $HOME/.latexmkrc
+
+# python 
+ln -sf $DOTFILES/python/condarc $HOME/.condarc
+ln -sf $DOTFILES/python/matplotlibrc $CONFIG/matplotlib/matplotlibrc
+
+# shell
+ln -sf $DOTFILES/shell/gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES/shell/zshrc $HOME/.zshrc
+
+# tmux
+ln -sf $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+ln -sf $DOTFILES/tmux/tmuxinator $CONFIG/
+
+# vim
+ln -sf $DOTFILES/vim/init.vim $CONFIG/nvim/init.vim
+ln -sf $DOTFILES/vim/spell $CONFIG/nvim/spell
+ln -sf $DOTFILES/vim/templates $CONFIG/nvim/templates
+ln -sf $DOTFILES/vim/ultisnips $CONFIG/nvim/ultisnips
+
